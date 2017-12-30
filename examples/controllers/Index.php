a8cedf8febdc9af3c4f0bdbd69d2735808d5c54d
zhangjie <zhangjie@chelun.com>
2017-12-30 15:07:12 +0800

    smallphp framework

--- examples/controllers/Index.php
+++ examples/controllers/Index.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 class Index extends \Smallphp\Mvc\Controller {
 	
 	public function index() {
-		$db = \Smallphp\Di::get('db');
+		$view = \Smallphp\Di::get('view');
+		$view->assign('title', 'Hello World');
+		$view->render('index/index.php');
 	}
 }
