938f9e96a99f223fd6bf4c13ed90ed8ef45582f3
zhangjie <zhangjie@chelun.com>
2017-12-30 15:48:14 +0800

    smallphp framework

--- Mvc/Model.php
+++ Mvc/Model.php
@@ -2,5 +2,12 @@
 namespace Smallphp\Mvc;
 
 abstract class Model {
+	
+	protected $db = NULL;
 
+	public function __construct() {
+		if ($this->db === NULL) {
+			$this->db = new \Smallphp\Database();
+		}
+	}
 }
